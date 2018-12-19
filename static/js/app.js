a5bfa22b5db1dacfc29709d74fd786455b5810bc
Christie Lafrance <christie.lafrance@gmail.com>
2018-12-18 16:59:59 -0800

    add comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,59 +1,56 @@
-function countryInfo(country) {
+// function countryInfo(country) {
 
-	let url = `/country_info/${country}`;
+// 	let url = `/country_info/${country}`;
 
-	d3.json(url).then(function(data) {
+// 	d3.json(url).then(function(data) {
 
-		// Select the panel with id of `#country-metadata`
-		var panel = d3.select("#country-metadata");
+// 		// Select the panel with id of `#country-metadata`
+// 		var panel = d3.select("#country-metadata");
 
-		// Clear any existing metadata
-    panel.html("");
+// 		// Clear any existing metadata
+//     panel.html("");
 
-    var div = panel.append("div");
-    Object.entries(data).forEach(function([key, value]) {
-      if (!value) {
-        div.append("p").text(`${key}: N/A`);
-      } else {
-        div.append("p").text(`${key}: ${value}`);
-      }    	
-    });
+//     var div = panel.append("div");
+//     Object.entries(data).forEach(function([key, value]) {
+//       if (!value) {
+//         div.append("p").text(`${key}: N/A`);
+//       } else {
+//         div.append("p").text(`${key}: ${value}`);
+//       }    	
+//     });
 
 
-    //// Call some function to build chart
-	});
-}
+//     //// Call some function to build chart
+// 	});
+// }
 
-// Function to build new charts when select a sample value
+
+// Function to build new charts when select a country
 function optionChanged(newSample) {
   // buildCharts(newSample);
   // buildMetadata(newSample);
 }
 
-// // Function to initilize the page
-// function init() {
 
-// 	var selector = d3.select("#selCountry");
+// Function to initilize the page
+function init() {
 
-// 	d3.json("/countries").then((countries) => {
-// 		countries.forEach((country) => {
-// 			selector
-// 				.append("option")
-// 				.text(country)
-// 				.property("value", country);
-// 		});
+	var selector = d3.select("#selCountry");
 
-// 		// // Use the first country from the list to build the initial plots
-//   //   const firstCountry = countries[0];
-//   //   buildCharts(firstCountry);
-//   //   buildMetadata(firstCountry);
-// 	});
-// }
+	d3.json("/countries").then((countries) => {
+		countries.forEach((country) => {
+			selector
+				.append("option")
+				.text(country)
+				.property("value", country);
+		});
 
-
-
-
-
+	// Use the first country from the list to build the initial plots
+  //   const firstCountry = countries[0];
+  //   buildCharts(firstCountry);
+  //   buildMetadata(firstCountry);
+	});
+}
 
 
 
