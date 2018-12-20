a96047e0929ba1be2b35abcf8aa20a27607613bf
Christie Lafrance <christie.lafrance@gmail.com>
2018-12-20 14:38:14 -0800

    add map basic, make changes on index.html

--- static/js/logic3.js
+++ static/js/logic3.js
@@ -1,7 +1,19 @@
-// console.log("Hello");
+// Creating our initial map object
+// We set the longitude, latitude, and the starting zoom level
+// This gets inserted into the div with an id of 'map'
+var myMap = L.map("map", {
+  center: [45.52, -122.67],
+  zoom: 13
+});
 
-// var svg = d3.select("#svg1")
-// 						.append("svg")
-// 						.attr("height", "500px")
-// 						.attr("width", "700px")
-// 						.attr("style", "outline: thin solid red");
+// Adding a tile layer (the background map image) to our map
+// We use the addTo method to add objects to our map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+
+console.log("hi");
